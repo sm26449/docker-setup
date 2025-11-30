2c89edcab66fdeb11ea113f15e9cad1f9cd243f0
sm26449 <152749411+sm26449@users.noreply.github.com>
2025-11-30 23:03:08 +0200

    remove surrounding quotes

--- lib/services.sh
+++ lib/services.sh
@@ -1707,11 +1707,8 @@ run_service_hooks() {
         # Collect commands (lines starting with "    - ")
         if $in_target_hook && [[ "$line" == "    - "* ]]; then
             local cmd="${line#    - }"
-            # Remove surrounding quotes (double or single)
             cmd="${cmd#\"}"
             cmd="${cmd%\"}"
-            cmd="${cmd#\'}"
-            cmd="${cmd%\'}"
             commands+=("$cmd")
         fi
     done < "$template"
